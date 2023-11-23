bbf937f78075b650120763181f05cd3edc836733
iorn121 <wmt.tkn.121@gmail.com>
2023-11-24 02:45:18 +0900

    consider array size

--- main.go
+++ main.go
@@ -14,9 +14,14 @@ func main() {
 	lambda.Start(HandleRequest)
 }
 
-func HandleRequest(ctx context.Context) (*string, error) {
+type MyEvent struct {
+	Name string `json:"name"`
+}
 
-	var params map[string]interface{}
+func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
+	fmt.Printf("start %s excecution", event.Name)
+
+	var params map[string]interface{} = make(map[string]interface{})
 	today := time.Now().Format("2006-01-02")
 	params["date"] = today
 	existed := kintone.ExistedIndex(today)
@@ -33,25 +38,31 @@ func HandleRequest(ctx context.Context) (*string, error) {
 	params["restingHeartRate"] = heartData.ActivitiesHeart[0].Value.RestingHeartRate
 
 	breathData := fitbit.BreathingRate(today)
-	params["deepSleepBR"] = breathData.BR[0].Value.DeepSleepSummary.BreathingRate
-	params["remSleepBR"] = breathData.BR[0].Value.RemSleepSummary.BreathingRate
-	params["fullSleepBR"] = breathData.BR[0].Value.FullSleepSummary.BreathingRate
-	params["lightSleepBR"] = breathData.BR[0].Value.LightSleepSummary.BreathingRate
+	if len(breathData.BR) > 0 {
+		params["deepSleepBR"] = breathData.BR[0].Value.DeepSleepSummary.BreathingRate
+		params["remSleepBR"] = breathData.BR[0].Value.RemSleepSummary.BreathingRate
+		params["fullSleepBR"] = breathData.BR[0].Value.FullSleepSummary.BreathingRate
+		params["lightSleepBR"] = breathData.BR[0].Value.LightSleepSummary.BreathingRate
+	}
 
 	tempData := fitbit.SkinTemperature(today)
-	params["skinTemp"] = tempData.TempSkin[0].Value.NightlyRelative
+	if len(tempData.TempSkin) > 0 {
+		params["skinTemp"] = tempData.TempSkin[0].Value.NightlyRelative
+	}
 
 	sleepData := fitbit.SleepDetail(today)
-	params["sleepDuration"] = sleepData.Sleep[0].Duration
-	params["sleepEfficiency"] = sleepData.Sleep[0].Efficiency
-	params["sleepStartTime"] = sleepData.Sleep[0].StartTime
-	params["sleepEndTime"] = sleepData.Sleep[0].EndTime
-	params["timeInBed"] = sleepData.Sleep[0].TimeInBed
-	sleepLevels := []string{"wake", "light", "rem", "deep"}
-	for _, sl := range sleepLevels {
-		params[sl+"SleepCount"] = sleepData.Sleep[0].Levels.Summary[sl].Count
-		params[sl+"SleepMinutes"] = sleepData.Sleep[0].Levels.Summary[sl].Minutes
-		params[sl+"SleepThirtyDayAvgMinutes"] = sleepData.Sleep[0].Levels.Summary[sl].ThirtyDayAvgMinutes
+	if len(sleepData.Sleep) == 0 {
+		params["sleepDuration"] = sleepData.Sleep[0].Duration
+		params["sleepEfficiency"] = sleepData.Sleep[0].Efficiency
+		params["sleepStartTime"] = sleepData.Sleep[0].StartTime
+		params["sleepEndTime"] = sleepData.Sleep[0].EndTime
+		params["timeInBed"] = sleepData.Sleep[0].TimeInBed
+		sleepLevels := []string{"wake", "light", "rem", "deep"}
+		for _, sl := range sleepLevels {
+			params[sl+"SleepCount"] = sleepData.Sleep[0].Levels.Summary[sl].Count
+			params[sl+"SleepMinutes"] = sleepData.Sleep[0].Levels.Summary[sl].Minutes
+			params[sl+"SleepThirtyDayAvgMinutes"] = sleepData.Sleep[0].Levels.Summary[sl].ThirtyDayAvgMinutes
+		}
 	}
 
 	spO2Data := fitbit.SpO2(today)
@@ -60,11 +71,15 @@ func HandleRequest(ctx context.Context) (*string, error) {
 	params["SpO2Avg"] = spO2Data.Value.Avg
 
 	VO2Data := fitbit.VO2Max(today)
-	params["VO2Max"] = VO2Data.CardioScore[0].Value.Vo2Max
+	if len(VO2Data.CardioScore) > 0 {
+		params["VO2Max"] = VO2Data.CardioScore[0].Value.Vo2Max
+	}
 
 	HRVData := fitbit.HRV(today)
-	params["dailyHRV"] = HRVData.Hrv[0].Value.DailyRmssd
-	params["deepHRV"] = HRVData.Hrv[0].Value.DeepRmssd
+	if len(HRVData.Hrv) == 0 {
+		params["dailyHRV"] = HRVData.Hrv[0].Value.DailyRmssd
+		params["deepHRV"] = HRVData.Hrv[0].Value.DeepRmssd
+	}
 
 	// existed がサイズ0の場合
 	if len(existed) == 0 {
@@ -74,5 +89,6 @@ func HandleRequest(ctx context.Context) (*string, error) {
 	} else {
 		return nil, fmt.Errorf("Today's data is duplicated")
 	}
+	fmt.Printf("end %s excecution", event.Name)
 	return nil, nil
 }
